6c75f1265dd21f92a08e0d149638726ae374fc59
[Koosha Moradpour] <kushamoradpour@gmail.com>
2025-02-12 15:25:34 -0500

    footer

--- src/PortfolioContainer.jsx
+++ src/PortfolioContainer.jsx
@@ -5,7 +5,7 @@ import About from './components/pages/About';
 import Portfolio from './components/pages/Portfolio';
 import Contact from './components/pages/Contact';
 import Resume from './components/pages/Resume';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 
 
